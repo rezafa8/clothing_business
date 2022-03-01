0c73733549f426215075597c69fb05ceb5fce49f
rezafa8 <danahanseb@gmail.com>
2022-03-01 05:03:25 +0100

    update navigation using withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+
 import MenuItem from "../menu-item/menu-item.component";
 
 import './directory.styles.scss'
@@ -47,8 +48,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu"> {
-                this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))
             }
             </div>
